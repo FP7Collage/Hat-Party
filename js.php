bca2488b1241bc40329c31c352055e799c8197fc
Gvidas <gvidas@playgen.com>
2014-03-06 16:22:13 +0200

    Convert js echo file into js with embedded php

--- js.php
+++ js.php
@@ -1,6 +1,6 @@
-<?php echo '<script type="text/javascript">
+<script type="text/javascript">
 	objImage = new Image();
-	objImage.src="' . $vms['waitIcon'] . '";
+	objImage.src="<?= $vms['waitIcon'] ?>";
 			function update(matchClass,content){
     var elems = document.getElementsByTagName("*"),i;
     for (i in elems)
@@ -13,7 +13,7 @@
     }
 function loadXMLDoc(post_id, key, action)
 {
-	var text = "<div class=\'waitIcon\'></div><div class=\'waitText flagText\'><div class=\'waitText-left\'></div><div class=\'waitText-text\'>' . $vms['waitText'] . '</div><div class=\'waitText-right\'></div>";
+	var text = "<div class=\'waitIcon\'></div><div class=\'waitText flagText\'><div class=\'waitText-left\'></div><div class=\'waitText-text\'><?= $vms['waitText'] ?></div><div class=\'waitText-right\'></div>";
 var xmlhttp;
 if (window.XMLHttpRequest)
   {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -27,14 +27,14 @@ xmlhttp.onreadystatechange=function()
   {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    	update("flag-arya-"+post_id, xmlhttp.responseText); 
+    	update("flag-arya-"+post_id, xmlhttp.responseText);
     }
   else
 	{
-			update("flag-arya-"+post_id, text); 
+			update("flag-arya-"+post_id, text);
 	}
   }
-xmlhttp.open("GET","' . $vmpath . 'processor.php?action="+action+"&post_id="+post_id+"&key="+key,true);
+xmlhttp.open("GET","<?= $vmpath ?>processor.php?action="+action+"&post_id="+post_id+"&key="+key,true);
 xmlhttp.send();
 }
-</script>'; ?>
+</script>
