f84e317f05dbed6f214fce325c7d985a7dae81ba
Lex Robinson <lex@playgen.com>
2014-03-06 16:31:35 +0200

    beautify the css

--- css.php
+++ css.php
@@ -3,83 +3,94 @@ function default_css() {
 	$vmpath = WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), "", plugin_basename(__FILE__));
 	return <<<CSS
 //////////////////////////////////Start Here/////////////////////////////
-.flag, .unflag {
-	border:none;
-	background-color:transparent;
-	float:left;
+.flag,
+.unflag {
+	border: none;
+	background-color: transparent;
+	float: left;
 }
 
 .flag {
-	background-image:url({flag image});
-	background-repeat:no-repeat;
-	width:30px;
-	height:40px;
-	border:none;
-	background-color:transparent;
-	float:left;
-		cursor:pointer;
+	background-image: url({flag image});
+	background-repeat: no-repeat;
+	width: 30px;
+	height: 40px;
+	border: none;
+	background-color: transparent;
+	float: left;
+	cursor: pointer;
 }
 
 .unflag {
-	background-image:url({unflag image});
-	background-repeat:no-repeat;
-	width:30px;
-	height:40px;
-	border:none;
-	background-color:transparent;
-	float:left;
-		cursor:pointer;
+	background-image: url({unflag image});
+	background-repeat: no-repeat;
+	width: 30px;
+	height: 40px;
+	border: none;
+	background-color: transparent;
+	float: left;
+	cursor: pointer;
 }
 
-.waitIcon{
-	background-image:url({wait image});
-	background-repeat:no-repeat;
-	width:32px;
-	height:40px;
-	border:none;
-	background-color:transparent;
-	float:left;
+.waitIcon {
+	background-image: url({wait image});
+	background-repeat: no-repeat;
+	width: 32px;
+	height: 40px;
+	border: none;
+	background-color: transparent;
+	float: left;
 }
 
 .flagText {
-	float:left;
+	float: left;
 }
 
 .flag-arya {
-	display:inline;
-	float:left;
+	display: inline;
+	float: left;
 }
 
-.flag-button-text, .unflag-button-text {
-	display:none;
+.flag-button-text,
+.unflag-button-text {
+	display: none;
 }
 
-.unflagText-left, .waitText-left, .flagText-left {
-	background:url({$vmpath}images/text-side.png);
-	width:12px;
-	height:30px;
-	float:left;
+.unflagText-left,
+.waitText-left,
+.flagText-left {
+	background: url({$vmpath}images/text-side.png);
+	width: 12px;
+	height: 30px;
+	float: left;
 }
-.unflagText-right, .waitText-right, .flagText-right {
-	background:url({$vmpath}images/text-side.png);
-	background-repeat:no-repeat;
-	background-position:-15px;
-	height:30px;
-	width:6px;
-	float:left;
+
+.unflagText-right,
+.waitText-right,
+.flagText-right {
+	background: url({$vmpath}images/text-side.png);
+	background-repeat: no-repeat;
+	background-position: -15px;
+	height: 30px;
+	width: 6px;
+	float: left;
 }
-.unflagText-text, .flagText-text, .waitText-text {
-	display:inline-block;
-	background-image:url({$vmpath}images/text-center.png);
-	background-repeat:repeat-x;
-	height:30px;
-	background-position:center top;
+
+.unflagText-text,
+.flagText-text,
+.waitText-text {
+	display: inline-block;
+	background-image: url({$vmpath}images/text-center.png);
+	background-repeat: repeat-x;
+	height: 30px;
+	background-position: center top;
 	color: green;
 	float: left;
 	font-weight: bold;
 }
-.flag-arya-top{
-	float:right;
+
+.flag-arya-top {
+	float: right;
 }
 CSS;
 };
